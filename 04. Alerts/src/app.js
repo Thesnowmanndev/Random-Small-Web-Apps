32c435e09eb56ca735b71efe850a9f3da8408892
Kyle Martin <kyle.martin.0819@gmail.com>
2022-06-26 15:50:52 -0400

    Created Alert App

--- 04. Alerts/src/app.js
+++ 04. Alerts/src/app.js
@@ -1,5 +1,6 @@
 const navbar = document.getElementById("navbar");
 const app = document.getElementById("app-content");
+const deleteElement = document.getElementById("delete-container");
 const footer = document.getElementById("footer");
 
 const navbarContent = `
@@ -31,6 +32,26 @@ const appContent = `
         <h2 class="text-sm mt-10 underline">04. Alerts:</h2>
         <p class="mt-2">This is a simple web app to simulate a user attempting to delete "content" from the app via a button click. If the user inputs the correct word, the app will successfully "delete" content when the delete button is pushed. If the user inputs the wrong word or no word at all the app will alert the user to input the correct word.</p>
     </div>
+    <div class="mt-4 md:mt-8 px-2">
+    
+    <div class="py-12">
+        <div class="block">
+            <label for="confirm-delete">Confirm deletion:</label>
+            <input
+                type="text"
+                name="confirm-delete"
+                id="confirm-delete"
+                class="mt-1 block rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-slate-900 font-bold"
+                placeholder="Please type DELETE"
+            />
+        </div>
+        <button class="mt-4 text-xl border border-red-500 px-4 py-1 rounded-md" onClick="userDelete();"><i class="fas fa-trash text-red-600"></i></button>
+        <button class="ml-6 mt-4 text-xl border border-sky-500 px-4 py-1 rounded-md" onclick="location.reload();"><i class="fas fa-redo text-sky-500"></i></button>
+    </div>
+`;
+
+const deletedContent = `
+    <h1>Element Deleted.</h1>
 `;
 
 const footerContent = `
@@ -43,6 +64,19 @@ const footerContent = `
         </a>
     </p>
 `;
+
+let userDelete = () => {
+  let userInput = document.getElementById("confirm-delete").value.trim();
+  let deleteKeyword = "DELETE";
+
+  if (userInput == deleteKeyword) {
+    showDelete();
+  } else {
+    alert("Please type DELETE in the input box.");
+  }
+};
+
+let showDelete = () => (deleteElement.innerHTML = deletedContent);
 
 let writeNavbar = () => (navbar.innerHTML = navbarContent);
 let writeApp = () => (app.innerHTML = appContent);
