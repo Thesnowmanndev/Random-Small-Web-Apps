8bc9861085947d3b285ade625972ea1359ea0ab0
Kyle Martin <kyle.martin.0819@gmail.com>
2022-07-08 15:25:19 -0400

    Initialize App
    Start the foundations for the Small App. Ensure that it works and all elements are changed to represent JavaScript Classes application, not template app.

---
+++ 12. JS-Classes/src/app.js
@@ -0,0 +1,16 @@
+import {
+  navbar,
+  appHead,
+  appContent,
+  footer,
+  navbarContent,
+  appHeadContent,
+  appBodyContent,
+  footerContent,
+  createApp,
+} from "./components/appMarkup.js";
+
+createApp();
+
+// Debug
+let logClicked = () => console.log("Clicked");
